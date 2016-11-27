854a89371605df2ea2b08b4b8a9de980b7b76c0a
ThuanLC <thuanlcse61425@fpt.edu.com>
2016-11-27 14:35:59 +0700

    server

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ require('babel-register')({
         "presets": ["es2015", "react"]
 });
 
-var mongoose = require('./config/mongoose'),
-	express = require('./config/express');
+// var mongoose = require('./config/mongoose'),
+// 	express = require('./config/express');
 
-var mongo_db = mongoose();
+// var mongo_db = mongoose();
 
-var server = express(mongo_db);
+// var server = express(mongo_db);
 
 server.listen('3013');
 console.log('-----Server running at http://localhost:3013/ ------'); 
