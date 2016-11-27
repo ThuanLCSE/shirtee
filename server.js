30f2f176c3fd498afb1d2c8b5fd3ddf18b3bd8b3
ThuanLC <thuanlcse61425@fpt.edu.com>
2016-11-27 14:34:19 +0700

    server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-server.js
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+require('babel-register')({
+        "presets": ["es2015", "react"]
+});
+
+var mongoose = require('./config/mongoose'),
+	express = require('./config/express');
+
+var mongo_db = mongoose();
+
+var server = express(mongo_db);
+
+server.listen('3013');
+console.log('-----Server running at http://localhost:3013/ ------'); 
+module.exports = server;
