03fb1d691f1602e14d550ff8e632a9e0aa5d2da4
ThuanLC <thuanlcse61425@fpt.edu.com>
2016-11-27 14:53:58 +0700

    server

--- server/model/user.js
+++ server/model/user.js
@@ -7,25 +7,7 @@ var userSchema = new Schema({
         trim: true,
         default: '',
         required: 'Name required'
-      },
-	phone : {
-        type: String,
-        trim: true,
-        default: '',
-        required: 'Phone number required'
-      },
-	address : String,
-	IP : {
-	    type: String,
-	    default: '',
-	    trim: true,
-	    required: 'IP required'
-	  },
-	bookingNumber: Number,
-	create: {
-	    type: Date,
-	    default: Date.now
-	  }
+      }
 });
 
 mongoose.model('User', userSchema);
--- server/router/user.js
+++ server/router/user.js
@@ -1,11 +1,2 @@
 module.exports = function(app){
-	var userCtrller = require('../controller/user.ctrl');
-	var bookingTimeCtrler = require('../controller/bookingTime.ctrl');
-	app.route('/api/patient/register')
-	.post(userCtrller.checkIPNotExist,userCtrller.register);
-	app.route('/api/patient/cancel').get(userCtrller.cancelBookingByIP);
-	app.route('/api/patient/nearShift').get(bookingTimeCtrler.getNearestShift);
-	app.route('/api/patient/edit').post(userCtrller.editRegisterInfo);
-	app.route('/api/patient/current').get(userCtrller.getBookingAmount);
-	app.route('/api/patient/info').get(userCtrller.getRegisterInfo);
 }
