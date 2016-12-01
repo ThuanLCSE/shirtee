689c0302e61f10615f8e0d0a42b5239026eb46ff
ThuanLCSE <thuanlcse61425@fpt.edu.vn>
2016-12-01 19:30:46 +0700

    cattttt

---
+++ server/model/category.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;  
+
+var catSchema = new Schema({ 
+    name: {
+    	type: String,
+    	require: "name required"        
+    } 
+});
+
+var Category = mongoose.model('Category', catSchema);
+
+// var catlife = new Category({
+// 	name: 'Life style' 
+// });
+// catlife.save(function (err, cat) {
+//   if (err) {
+//   	console.log(err);
+// 	} else  {
+// 		console.log(cat);
+// 	}
+// });
+// catlife = new Category({
+// 	name: 'Music' 
+// });
+// catlife.save(function (err, cat) {
+//   if (err) {
+//   	console.log(err);
+// 	} else  {
+// 		console.log(cat);
+// 	}
+// });
+// catlife = new Category({
+// 	name: 'Artist' 
+// });
+// catlife.save(function (err, cat) {
+//   if (err) {
+//   	console.log(err);
+// 	} else  {
+// 		console.log(cat);
+// 	}
+// }); 
