060505a6398748ce6fb27798d7c742a8e8b4f8f3
ThuanLCSE <thuanlcse61425@fpt.edu.vn>
2016-12-07 20:42:33 +0700

    upload pattern url

--- server/router/designer.js
+++ server/router/designer.js
@@ -5,7 +5,7 @@ module.exports = function(app){
 	app.route('/api/designer/register')
 	.post(designerCtrller.checkDesignerNotExist, designerCtrller.register); 
 	app.route('/api/designer/pattern/create/:designerId')
-	.post(designerCtrller.getById, patternCtrller.create); 
+	.post(designerCtrller.getById,designerCtrller.getLevelById, patternCtrller.create); 
 	app.route('/api/designer/pattern/listAll/:designerId')
 	.get(designerCtrller.getById, patternCtrller.getAllOfDesigner); 
 
