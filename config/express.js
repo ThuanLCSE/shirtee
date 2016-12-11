895a011c0f19faa6abffd45a0835cbc44a11558b
ThuanLCSE <thuanlcse61425@fpt.edu.vn>
2016-12-11 13:46:18 +0700

    update riouter

--- config/express.js
+++ config/express.js
@@ -38,6 +38,7 @@ module.exports = function(database) {
 	require('../server/router/user.js')(app);  
 		require('../server/router/admin.js')(app);  
  	require('../server/router/designer.js')(app);  
+ 	require('../server/router/saleProgram.js')(app);  
 	require('../server/router/common.js')(app); 
 
 	return server;
